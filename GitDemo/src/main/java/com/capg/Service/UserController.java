e17c67c7b17de37f1eb4fc15c803b55793562eff
Hp <Hp@DESKTOP-I4J9DP4>
2022-03-24 00:06:28 +0530

    New Commit in master

--- GitDemo/src/main/java/com/capg/Service/UserController.java
+++ GitDemo/src/main/java/com/capg/Service/UserController.java
@@ -1,8 +1,10 @@
 package com.capg.Service;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/home")
 public class UserController {
 
 	public void test()
